2eaaf16dfe55fc54f79f043656179f459cd78865
kodigantivandu <153985170+kodigantivandu@users.noreply.github.com>
2023-12-16 20:50:15 +1000

    Added first set of file

---
+++ VanduDecember2023/VanduDecember2023/LaunchTurnUpPortal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace VanduDecember2023
+{
+    class LaunchTurnUpPortal
+    {
+        public static void LaunchAppandLogintoApplication()
+        {
+
+            //Open the Chrome browser
+            IWebDriver driver = new ChromeDriver();
+            //Maximize the browser
+            driver.Manage().Window.Maximize();
+
+            //Launch TurnUp portal and navigate to website login page
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+            Console.WriteLine("2. Launch Application Successful");
+
+
+            //Identify username textbox and enter valid username
+            IWebElement usernameTextBox = driver.FindElement(By.Id("UserName"));
+            usernameTextBox.SendKeys("hari");
+
+            //Identify password textbox and enter valid password
+            IWebElement passwordTextBox = driver.FindElement(By.Id("Password"));
+            passwordTextBox.SendKeys("123123");
+
+            //Identify the login button and click on the button
+            IWebElement loginButton = driver.FindElement(By.XPath("//*[@id=\"loginForm\"]/form/div[3]/input[1]"));
+            loginButton.Click();
+
+            //Check if user has logged in successfully
+            IWebElement helloHari = driver.FindElement(By.XPath("//*[@id=\"logoutForm\"]/ul/li/a"));
+
+            if (helloHari.Text == "Hello hari!")
+            {
+                Console.WriteLine("User has logged in successfully");
+            }
+            else
+            {
+                Console.WriteLine("User hasn't been logged in.");
+            }
+
+            Console.WriteLine("3. Loginto TurnUp Application Successful");
+
+        }
+    }
+
+}
---
+++ VanduDecember2023/VanduDecember2023/MainMethod.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using VanduDecember2023;
+
+Console.WriteLine("--- 1. Test START ---");
+
+// *** Turn Up Application related Test calls: *** //
+
+//Launch TurnUp Portal
+LaunchTurnUpPortal.LaunchAppandLogintoApplication();
+
+
+
+Console.WriteLine("--- 4. Test START ---");
+
+
+/*
+//Other Misc Method for Practice
+
+int a = 1;
+int b = 2;
+
+
+
+// Call all methods her
+
+// Add Method
+Calc_Add.AddMethod(a, b);
+
+//Sub Method*/
