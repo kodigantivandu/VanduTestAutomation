4bdb3a4f368c3efec13e281f03231c401f2a8985
kodigantivandu <153985170+kodigantivandu@users.noreply.github.com>
2023-12-16 20:32:41 +1000

    Added first set of file

---
+++ VanduDecember2023/VanduDecember2023/Calc_Add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanduDecember2023
+{
+    public static class Calc_Add
+    {
+        public static void AddMethod(int a, int b)
+        {
+            int c = a + b;
+
+            System.Console.WriteLine("Added value of A+B = C is " + c);
+        }
+    }
+}
---
+++ VanduDecember2023/VanduDecember2023/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using VanduDecember2023;
+
+int a = 1;
+int b = 2;
+
+
+
+// Call all methods her
+
+// Add Method
+Calc_Add.AddMethod(a, b);
+
+//Sub Method
+
+
+//Launch Application and Login
+LaunchAppandLogin.LaunchAppandLogintoApplication();
+
+
